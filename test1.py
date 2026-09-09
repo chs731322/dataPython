e173e0886efdf966b1b1d885e7b7cf21b68a6c88
chs731322 <chohaesol@naver.com>
2026-09-09 10:16:40 +0900

    4 commit

--- test1.py
+++ test1.py
@@ -1,7 +1,10 @@
 name = "해소리"
 year = 2026
 score = 10000
+target_year = 2030
+future_job = "돈 많은 백수"
 
 print("이름 : ", name, ", 연도 : ", year, ", 점수 : ", score)
 # f는 Formatted String Literal, 즉 문자열 포맷팅을 의미
 print(f"{name}의 {year}년 예측 점수는 {score}점입니다.")
+print(f"{name}는 {target_year}에는 {future_job}로 살고 싶습니다.")
