cb6a38cbf0190b0ff83b4d21788b19873b4e5e15
chs731322 <chohaesol@naver.com>
2026-09-09 10:20:46 +0900

    계산기 commit

---
+++ test2.py
@@ -0,0 +1,13 @@
+print("=========================")
+print("계산기")
+print("=========================")
+
+name = input("이름을 입력하세요 : ")
+print(f"반갑습니다. {name}님!")
+
+a = int(input("첫 번째 숫자를 입력하세요 : "))
+b = int(input("두 번째 숫자를 입력하세요 : "))
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
