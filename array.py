7b0fa6ea54664f4b919aa6560562d22a881bd01c
chs731322 <chohaesol@naver.com>
2026-09-16 11:24:27 +0900

    숫자맞추기 게임2

--- array.py
+++ array.py
@@ -5,9 +5,21 @@ print("숫자 맞추기 게임")
 answer = random.randint(1, 100)
 
 while True :
-    a = int(input("숫자를 입력하세요 : "))
+    try:
+        a = int(input("숫자를 입력하세요 : "))
+    except ValueError:
+        print("1~100 숫자만 입력")
+        continue
+
+    if a < 1 or a > 100:
+        print("1~100사이의 숫자만 입력")
+        continue
+
+    n += 1
+
     if a == answer:
         print("정답입니다")
+        print(f"{n}회 만에 성공")
         break
     elif a > answer:
         print("down")
